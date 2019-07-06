3534b2e02c17df3b87c3dff0dfd6e06a62da2742
sjoerdgn <38759766+SjoerdGn@users.noreply.github.com>
2019-07-06 13:03:01 +0200

    yearly whatsapps

--- whatsapp/whatsapp.py
+++ whatsapp/whatsapp.py
@@ -11,26 +11,58 @@ import matplotlib.pyplot as plt
 
 
 import csv
-
+import collections
 
 #%%
 
 data = pd.read_csv("appdata.txt",  skip_blank_lines=True, delimiter = " ## ") #delimiter = " - ",
-                   
+        
 #%%
 
-
+countmessages = {}
 #%%
+count = 0
 for line in data["date - message"]:
-    nline = line.split(" - ")
-    try:
-        nline = nline[1].split(":")
-        print(nline[0])
-    except: 
-        print("No data")
-                   
+    count +=1
+    if count > 0: #33948:    #sinds 
+        nline = line.split(" - ")
+        try:
+            nline = nline[1].split(":")
+            if nline[0] in countmessages:
+                countmessages[nline[0]] += 1
+            else:
+                countmessages[nline[0]] = 1
+        except: 
+            pass
+            #print("No data")
+            
+print(countmessages)
                    #%%
-print(data.iloc[8701])
+
+cm = {i:countmessages[i] for i in countmessages if countmessages[i] > 10} 
+cm = sorted(cm.items(), key=lambda kv: kv[1], reverse=True)
+cm = collections.OrderedDict(cm)
+
+#%%
+
+fig = plt.figure(figsize = (15,15))  
+ax = fig.add_subplot(1, 1, 1)   
+
+major_ticks = np.arange(0, 8000, 500)
+minor_ticks = np.arange(0, 8000, 100)
+
+ax.set_yticks(major_ticks)
+ax.set_yticks(minor_ticks, minor=True)
+plt.grid(which = 'major', axis = 'y', linestyle = '-')
+plt.grid(which = 'minor', axis = 'y', linestyle = ':')   
+     
+plt.bar(cm.keys(), cm.values())
+plt.xticks(rotation = 90)
+
+
+plt.ylabel("Aantal whatsappjes")
+plt.title("Hoevaak hebben die mooie jongens dan gewhatsappt sinds 26 augustus 2018?")
+plt.savefig("Whatsappmesssent2.png", dpi = 200)
 
 #%%
 
@@ -39,3 +71,7 @@ data = csv.reader(file, delimiter = "-")
 for row in data:
     print(row)
 file.close()
+
+#%%
+
+print(sum(cm.values()))
